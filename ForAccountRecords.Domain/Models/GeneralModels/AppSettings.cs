3c52b023a30bc2954e1054271fdef5afd0353097
Rikudou En Sof <info@rikudouensof.com>
2023-11-17 12:27:33 +0100

    Implement asymetric encryption

--- ForAccountRecords.Domain/Models/GeneralModels/AppSettings.cs
+++ ForAccountRecords.Domain/Models/GeneralModels/AppSettings.cs
@@ -9,6 +9,10 @@ namespace ForAccountRecords.Domain.Models.GeneralModels
   public class AppSettings
   {
 
+    public string AsymetricEncryptPrivateKey { get; set; }
 
+    public string AsymetricEncryptPublicKey { get; set; }
+
+    public string SymetricEncryptKey { get; set; }
   }
 }
